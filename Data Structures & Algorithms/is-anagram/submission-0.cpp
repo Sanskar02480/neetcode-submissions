c6608ac5233aed8e075af969cbc47c60505bd87c
Sanskar Tolumbia <sanskartolumbia55@gmail.com>
2026-08-13 23:15:35 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!= t.size())
+        {
+            return false;
+        }
+        unordered_map<int, int>freq1;
+        unordered_map<int, int>freq2;
+        for(int i=0; i<s.size();i++)
+        {
+            freq1[s[i]]++;
+        }
+        for(int j=0; j<t.size();j++)
+        {
+            freq2[t[j]]++;
+        }
+        if(freq1==freq2)
+        {
+            return true;
+        }
+
+    return false;
+    
+    }
+};
